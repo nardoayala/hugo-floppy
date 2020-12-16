c75e0381922628ff4d188be58e0e4e5aa5077de7
Bernardo <ba@bernardoayala.com>
2020-12-15 21:45:40 -0400

    New design with paginator

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,28 +1,28 @@
-const posts = document.querySelector(".posts__list").querySelectorAll("li")
-const filterButtons = document.querySelectorAll(".filter-button")
+const posts = document.querySelectorAll(".post");
+const filterButtons = document.querySelectorAll(".filter-button");
 
-const filterButtonsList = [...filterButtons]
-const postsList = [...posts]
+const filterButtonsList = [...filterButtons];
+const postsList = [...posts];
 
 filterButtonsList.forEach(button => {
   button.addEventListener("click", event => {
-    button.classList.add("active")
+    button.classList.add("active");
     postsList.forEach(post => {
       const category = event.target.innerText.replace(" ","-")
       if (category === "Todos") {
-        post.classList.remove("hide")
+        post.classList.remove("hide");
       }
       else if (!post.classList.contains(category)) {
-        post.classList.add("hide")
+        post.classList.add("hide");
       } else {
-        post.classList.remove("hide")
+        post.classList.remove("hide");
       }
-    })
+    });
 
     filterButtonsList.map(button => {
       if (button != event.target) {
-        button.classList.remove("active")
+        button.classList.remove("active");
       }
-    })
-  })
-})
+    });
+  });
+});
