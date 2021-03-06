7aa0cc471231cffc732e86f06bcc3fca58294773
Bernardo <ba@bernardoayala.com>
2021-03-06 09:14:58 -0400

    Better filter function

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -4,25 +4,22 @@ const filterButtons = document.querySelectorAll(".filter-button");
 const filterButtonsList = [...filterButtons];
 const postsList = [...posts];
 
-filterButtonsList.forEach(button => {
-  button.addEventListener("click", event => {
-    button.classList.add("active");
-    postsList.forEach(post => {
-      const category = event.target.innerText.replace(" ","-")
-      if (category === "Todos") {
-        post.classList.remove("hide");
-      }
-      else if (!post.classList.contains(category)) {
-        post.classList.add("hide");
-      } else {
-        post.classList.remove("hide");
-      }
-    });
+function urlize(string) {
+  return ((string.replace(" ","-")).toLowerCase());
+}
 
-    filterButtonsList.map(button => {
-      if (button != event.target) {
-        button.classList.remove("active");
-      }
-    });
+function filter(button) {
+  filterButtonsList.forEach((button) => button.classList.remove("active"));
+  button.classList.add("active");
+
+  const category = urlize(button.innerText);
+  postsList.forEach((post) => {
+    if (category === "todos") {
+      post.classList.remove("hide");
+    } else if (!post.classList.contains(category)) {
+      post.classList.add("hide");
+    } else {
+      post.classList.remove("hide");
+    };
   });
-});
+};
