7e9edf40c1095d0788f8de7f9795e2cb95cda410
Bernardo Ayala <ba@bernardoayala.com>
2021-05-09 10:35:57 -0400

    Filter function getting name of buttons instead of innertext

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -4,17 +4,13 @@ const filterButtons = document.querySelectorAll(".filter-button");
 const filterButtonsList = [...filterButtons];
 const postsList = [...posts];
 
-function urlize(string) {
-  return ((string.replace(" ","-")).toLowerCase());
-}
-
 function filter(button) {
   filterButtonsList.forEach((button) => button.classList.remove("active"));
   button.classList.add("active");
 
-  const category = urlize(button.innerText);
+  const category = encodeURI(button.name);
   postsList.forEach((post) => {
-    if (category === "todos") {
+    if (category === "all") {
       post.classList.remove("hide");
     } else if (!post.classList.contains(category)) {
       post.classList.add("hide");
